82ce68e89e0d0d280b57e639bc047c0e2562ec52
salmansiddik19 <salmansiddik19@gmail.com>
2021-04-13 19:24:04 +0600

    upto test...

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import Product
 
-# Create your tests here.
+
+class BasicTest(TestCase):
+    def test_fields(self):
+        product = Product()
+        product.name = 'test item'
+        product.description = 'it is a test item'
+        product.price = 999
+        product.save()
+
+        record = Product.objects.get(pk=1)
+        self.assertEqual(record, product)
+
+    def test_slug_on_save(self):
+        product = Product()
+        product.name = 'test item'
+        product.description = 'it is a test item'
+        product.price = 999
+        product.save()
+
+        self.assertEqual(product.stock, False)
